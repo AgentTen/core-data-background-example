d65114fe403f44112b7d45dd169616d49f142a86
Ryan Farley <rfarley@wyzant.com>
2015-12-29 16:07:11 -0600

    Initial Commit

---
+++ ChicagoTransitDataImport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChicagoTransitDataImport
+//
+//  Created by Farley, Ryan on 12/29/15.
+//  Copyright © 2015 Farley, Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChicagoTransitDataImport/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChicagoTransitDataImport
+//
+//  Created by Farley, Ryan on 12/29/15.
+//  Copyright © 2015 Farley, Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
